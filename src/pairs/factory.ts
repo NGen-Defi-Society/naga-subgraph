846cdfdbc31f4682de08336513429810bf2c9dfb
suphasonnuk <suphasonnuk@gmai.com>
2021-08-05 13:50:16 +0700

    First commit

--- src/pairs/factory.ts
+++ src/pairs/factory.ts
@@ -5,7 +5,7 @@ import { PairCreated } from "../../generated/Factory/Factory";
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from "./utils/bep20";
 
 // Constants
-let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+let FACTORY_ADDRESS = "0x250570a200945860b3e76aB9f1Fa325f55CBE10C";
 
 // BigNumber-like references
 let ZERO_BI = BigInt.fromI32(0);
